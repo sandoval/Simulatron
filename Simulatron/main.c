3c77fe60137e913a2649ee1ee9b5519293dcc0ad
Daniel Sandoval <daniel@loopec.com.br>
2013-05-04 18:34:40 -0300

    Added .gitignore.

--- Simulatron/main.c
+++ Simulatron/main.c
@@ -7,12 +7,48 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "asmFunctions.h"
+
+short int accumulator;
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    unsigned short int codigo = 0;
+    unsigned long programCounter = 0;
+    short int* codeMemory = (short int*)malloc(2048*sizeof(short int));
+    short int* dataMemory = (short int*)malloc(2048*sizeof(short int));
+    accumulator = 0;
+    
+    printf("Entre manualmente o código do programa:\n");
+    
+    long i = 0;
+    while (1) {
+        scanf("%hud", &codigo);
+        if (codigo == 65535)
+            break;
+        codeMemory[i] = codigo;
+        i++;
+    }
+    
+    printf("Entre manualmente os dados de memória:\n");
+    
+    i = 0;
+    while (1) {
+        scanf("%hud", &codigo);
+        if (codigo == 65535)
+            break;
+        dataMemory[i] = codigo;
+        i++;
+    }
+    
+    asmFunctions[0] = soma;
+    asmFunctions[13] = stop;
+    
+    while (asmFunctions[codeMemory[programCounter]-1](&programCounter, codeMemory, dataMemory));
+    
+    printf("Resultado do acumulador: %hd", accumulator);
+    
     return 0;
 }
 
