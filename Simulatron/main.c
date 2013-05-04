53313f95bb9c88362edab74682b37224c0c5b5eb
Daniel Sandoval <daniel@loopec.com.br>
2013-05-04 17:53:22 -0300

    Initial Commit

---
+++ Simulatron/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Simulatron
+//
+//  Created by Daniel Sandoval on 04/05/2013.
+//  Copyright (c) 2013 LoopEC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
